510e3d49dffbd0951d17ffddfd0364d7c9c86847
Marcelo Mazetto <marcelomazetto@czbrains.com.br>
2020-12-15 18:05:17 -0300

    Added CORS to back-end

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const routes = require('./routes');
 
-app.use(routes);
-app.use(express.static('public'))
+const corsOptions = {
+    origin: 'http://localhost:8080',
+}
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!');
-});
+app
+    .use(cors(corsOptions))
+    .use(routes)
+    .use(express.static('public'))
+    .listen(3000, function () {
+        console.log('Listening on port 3000!');
+    });
